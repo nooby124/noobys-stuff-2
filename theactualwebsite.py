6c3898c5316a918031edbfcd054bdcb2b4130e7c
nooby124 <139121928+nooby124@users.noreply.github.com>
2023-07-21 23:03:27 +0300

    Update theactualwebsite.py

--- theactualwebsite.py
+++ theactualwebsite.py
@@ -13,7 +13,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 td = "---"
 
-tab0, tab1, tab2, tab3, tab4 = st.tabs([" ", "me", "gimme ideas im bored", "github page", "reviewing your ideas"])
+tab0, tab1, tab2, tab3, tab4 = st.tabs(["home", "me", "gimme ideas im bored", "github page", "reviewing your ideas"])
 
 with tab0:
     st.title('hello and welcome to noobys website!')
