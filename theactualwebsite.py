17bdace85e49b0ab812813168a3f4eabf2417f26
nooby124 <139121928+nooby124@users.noreply.github.com>
2023-07-23 19:14:24 +0300

    Update theactualwebsite.py

--- theactualwebsite.py
+++ theactualwebsite.py
@@ -4,9 +4,6 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-                        body {
-                 scroll-behavior: smooth;
-            }
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
